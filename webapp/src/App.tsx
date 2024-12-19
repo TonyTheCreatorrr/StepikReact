157f58f80ca9e0786e77fee269a5f70711aa7f09
TonyTheCreatorrr <toni.bakshi@mail.ru>
2024-12-19 15:53:05 +0300

    19-12-2024

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,21 +1,21 @@
 export const App = () => {
   const missions = [
     {
-      nick: "cool-mission-1",
-      name: "Mission",
-      description: "mission description 1",
+      nick: 'cool-mission-1',
+      name: 'Mission',
+      description: 'mission description 1',
     },
     {
-      nick: "cool-mission-2",
-      name: "Mission",
-      description: "mission description 2",
+      nick: 'cool-mission-2',
+      name: 'Mission',
+      description: 'mission description 2',
     },
     {
-      nick: "cool-mission-3",
-      name: "Mission",
-      description: "mission description 3",
+      nick: 'cool-mission-3',
+      name: 'Mission',
+      description: 'mission description 3',
     },
-  ];
+  ]
   return (
     <div>
       <h1>ToDo</h1>
@@ -25,8 +25,8 @@ export const App = () => {
             <h2>{mission.name}</h2>
             <p>{mission.description}</p>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
