88f60cbaaebb7a00bcf3ff024fffa4dca339bdcf
ToniPC <baksheevanthony@yandex.ru>
2024-12-19 19:49:02 +0300

    From home PC

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,18 +2,23 @@ export const App = () => {
   const missions = [
     {
       nick: 'cool-mission-1',
-      name: 'Mission',
+      name: 'Mission 1',
       description: 'mission description 1',
     },
     {
       nick: 'cool-mission-2',
-      name: 'Mission',
+      name: 'Mission 2',
       description: 'mission description 2',
     },
     {
       nick: 'cool-mission-3',
-      name: 'Mission',
+      name: 'Mission 3',
       description: 'mission description 3',
+    },
+    {
+      nick: 'cool-mission-4',
+      name: 'Mission 4',
+      description: 'mission description 4',
     },
   ]
   return (
