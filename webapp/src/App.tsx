e08a64e147a896804ab7ebf6cc525939b3a8e348
TonyTheCreatorrr <toni.bakshi@mail.ru>
2024-12-19 12:05:01 +0300

    revision 1

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
+  const missions = [
+    {
+      nick: "cool-mission-1",
+      name: "Mission",
+      description: "mission description 1",
+    },
+    {
+      nick: "cool-mission-2",
+      name: "Mission",
+      description: "mission description 2",
+    },
+    {
+      nick: "cool-mission-3",
+      name: "Mission",
+      description: "mission description 3",
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://youtube.com" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <h1>ToDo</h1>
+      {missions.map((mission) => {
+        return (
+          <div key={mission.nick}>
+            <h2>{mission.name}</h2>
+            <p>{mission.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
